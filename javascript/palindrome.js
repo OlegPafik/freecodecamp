ed8cf4f7db39125addfc2170dbbff1176c9da598
Oleg <oleg.pafik@eversens.com>
2023-04-20 21:12:51 +0200

    docs(palindrome): Add JSDoc

--- javascript/palindrome.js
+++ javascript/palindrome.js
@@ -1,3 +1,8 @@
+/**
+ * Checks if str is a palindrome (case sensitive), when str has no special symbols.
+ * @param {string} str - The word to check.
+ * @returns {boolean} True or False.
+ */
 function palindrome(str) {    
   const forwardStr = str
   const forwardLetters = str.split("");
