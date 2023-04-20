f4fb6e337d6adb6fcbdd7ff07c72f3362bc0996d
Oleg <oleg.pafik@eversens.com>
2023-04-20 21:05:09 +0200

    style(palindrome): Remove unnecesary leading indentation

--- javascript/palindrome.js
+++ javascript/palindrome.js
@@ -1,9 +1,9 @@
-function palindrome(str) {
-    const forwardStr = str
-    const forwardLetters = str.split("");
-    const reversedStr = forwardLetters.reverse().join("")
-    const isPalindrome = forwardStr == reversedStr
-    return isPalindrome
-  }
+function palindrome(str) {    
+  const forwardStr = str
+  const forwardLetters = str.split("");
+  const reversedStr = forwardLetters.reverse().join("")
+  const isPalindrome = forwardStr == reversedStr
+  return isPalindrome
+}
   
-  console.log(palindrome("eye"));
+console.log(palindrome("eye"));
