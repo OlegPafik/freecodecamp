432d55e07848c30891a02901a4663295f916b31a
Oleg <oleg.pafik@eversens.com>
2023-04-23 01:50:08 +0200

    docs(palindrome): Update JSDoc of palindrome()

--- javascript/palindrome.js
+++ javascript/palindrome.js
@@ -1,5 +1,5 @@
 /**
- * Checks if str is a palindrome (case sensitive), when str has no special symbols.
+ * Checks if str is a palindrome, case and special symbols insensitive.
  * @param {string} str - The word to check.
  * @returns {boolean} True or False.
  */
@@ -8,7 +8,7 @@ function palindrome(str) {
   function reverse(str) {
     return str.split("").reverse().join("")
   }
-  function removeNonAlphanumerics(str){
+  function removeNonAlphanumerics(str) {
     return str.replace(/[^a-z0-9]/gi,"")
   }
   
