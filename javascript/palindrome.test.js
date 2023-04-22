d55a9df7488b85a38959849a3261d2163adec11f
Oleg <oleg.pafik@eversens.com>
2023-04-23 01:47:02 +0200

    test(palindrome): Complete test suite with tests from freeCodeCamp

--- javascript/palindrome.test.js
+++ javascript/palindrome.test.js
@@ -15,6 +15,8 @@ test('Basic palindrome as "eyed" that are false', () => {
   expect(palindrome("eyed")).toBe(false)
   expect(palindrome("racecard")).toBe(false)
   expect(palindrome("EYED")).toBe(false)
+  expect(palindrome("nope")).toBe(false)
+  expect(palindrome("almostomla")).toBe(false)
 })
 
 test('Special palindrome as "_eye" that are true', () => {
@@ -24,7 +26,15 @@ test('Special palindrome as "_eye" that are true', () => {
   expect(palindrome(" E Y    E ")).toBe(true)
   expect(palindrome(" 2   5 2 ")).toBe(true)
   expect(palindrome("2_A3*3#A2.*")).toBe(true)
+  expect(palindrome("never odd or even")).toBe(true)
+  expect(palindrome("0_0 (: /-\ :) 0-0")).toBe(true)
 })
+
+test('Special palindrome as "not a palindrome" that are false', () => {
+  expect(palindrome("not a palindrome")).toBe(false)
+  expect(palindrome("1 eye for of 1 eye.")).toBe(false)
+  expect(palindrome("five|\_/|four")).toBe(false)
+}) 
 
 test('Case insensitive basic palindrome that are true', () => {
   expect(palindrome("eYE")).toBe(true)
@@ -34,4 +44,7 @@ test('Case insensitive special palindrome that are true', () => {
   expect(palindrome("race CAR")).toBe(true)
   expect(palindrome("2A3*3a2")).toBe(true)
   expect(palindrome("2A3 3a2")).toBe(true)
+  expect(palindrome("A man, a plan, a canal. Panama")).toBe(true)
+  expect(palindrome("My age is 0, 0 si ega ym.")).toBe(true)
 })
+
