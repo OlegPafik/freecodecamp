d65d4b6ddf1bf964ceca3796757435c7c2b04b68
Oleg <oleg.pafik@eversens.com>
2023-04-23 01:37:09 +0200

    test(palindrome): Complete test suite with own tests and tests suggested in the excercise description

--- javascript/palindrome.test.js
+++ javascript/palindrome.test.js
@@ -1,14 +1,37 @@
-const palindrome = require("./palindrome");
-
-test('"eye" is true', () => {
-  expect(palindrome("eye")).toBe(true);
-});
+const palindrome = require("./palindrome")
 
 test("palindrome() is boolean", () => {
-    expect(palindrome("eye")).toBe(true);
-    expect(palindrome("eyed")).toBe(false);
-  });
+  expect(palindrome("eye")).toBe(true)
+  expect(palindrome("eyed")).toBe(false)
+})
 
-test('"_eye" is true', () => {
-    expect(palindrome("_eye")).toBe(true);
+test('Basic palindrome as "eye" that are true', () => {
+  expect(palindrome("eye")).toBe(true)
+  expect(palindrome("racecar")).toBe(true)
+  expect(palindrome("EYE")).toBe(true)
+})
+
+test('Basic palindrome as "eyed" that are false', () => {
+  expect(palindrome("eyed")).toBe(false)
+  expect(palindrome("racecard")).toBe(false)
+  expect(palindrome("EYED")).toBe(false)
+})
+
+test('Special palindrome as "_eye" that are true', () => {
+  expect(palindrome("_eye")).toBe(true)
+  expect(palindrome("_eye__")).toBe(true)
+  expect(palindrome("_EYE")).toBe(true)
+  expect(palindrome(" E Y    E ")).toBe(true)
+  expect(palindrome(" 2   5 2 ")).toBe(true)
+  expect(palindrome("2_A3*3#A2.*")).toBe(true)
+})
+
+test('Case insensitive basic palindrome that are true', () => {
+  expect(palindrome("eYE")).toBe(true)
+})
+
+test('Case insensitive special palindrome that are true', () => {
+  expect(palindrome("race CAR")).toBe(true)
+  expect(palindrome("2A3*3a2")).toBe(true)
+  expect(palindrome("2A3 3a2")).toBe(true)
 })
