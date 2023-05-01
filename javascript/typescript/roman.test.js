46280e2369953b6a20ad2d5deb9bef16d75284e0
Oleg <oleg.pafik@eversens.com>
2023-05-01 13:27:38 +0200

    test(roman): Add test 4 is IV and 5 is V

--- javascript/typescript/roman.test.js
+++ javascript/typescript/roman.test.js
@@ -7,3 +7,11 @@ test("2 is II", () => {
 test("3 is III", () => {
   expect(roman(3)).toBe("III")
 })
+
+test("4 is IV", () => {
+  expect(roman(4)).toBe("IV")
+})
+
+test("5 is V", () => {
+  expect(roman(5)).toBe("V")
+})
