ef39a6183e4b19b7b26d9ca8e54957f5539998d3
Oleg <oleg.pafik@eversens.com>
2023-05-01 13:22:11 +0200

    refactor(roman): Change the function declaration to an arrow function

--- javascript/typescript/roman.ts
+++ javascript/typescript/roman.ts
@@ -3,13 +3,13 @@
  * @param {number} number - The arabic number to convert.
  * @returns {string} - The resulting roman numeral.
  */
-function roman(number: number): string {
+const roman = (number: number): string => {
   let dict: { [arabic: number]: string } = {
     1: "I"
   }
 
   let roman: string = ""
-  
+
   for (let i = 0; i < number; i++) {
     roman = roman + dict[1]
   }
